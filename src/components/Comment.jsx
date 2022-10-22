c02687a29597f388519f6c152ffb9beb1ed59c36
dandevweb <danilo.pacheco@soitic.com>
2022-10-22 11:46:29 -0300

    Add function to delete comment and textarea validation

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -2,7 +2,10 @@ import { ThumbsUp, Trash } from 'phosphor-react';
 import { Avatar } from './Avatar';
 import styles from './Comment.module.css';
 
-export function Comment({ content }) {
+export function Comment({ content, onDeleteComment }) {
+    function handleDeleteComment() {
+        onDeleteComment(content)
+    }
     return (
         <div className={styles.comment}>
             <Avatar hasBorder={false} src='https://github.com/dandevweb.png' />
@@ -15,7 +18,7 @@ export function Comment({ content }) {
                             <time title='18 de outubro às 16h' dateTime='2022-10-18 16:00:00'>Cerca de 1h atrás</time>
                         </div>
 
-                        <button title='Deletar comentário'>
+                        <button onClick={handleDeleteComment} title='Deletar comentário'>
                             <Trash size={24} />
                         </button>
                     </header>
